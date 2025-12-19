c74837e6f0fd1fa3d94a3cce713e3c4e04495374
Laszlo Fogas <laszlo@gimlet.io>
2025-12-19 12:27:09 +0100

    chore: update Homebrew formula to 0.14.0-rc.3

--- Formula/capacitor.rb
+++ Formula/capacitor.rb
@@ -1,13 +1,13 @@
 class Capacitor < Formula
   desc "Kubernetes GUI for GitOps with Flux and Helm"
   homepage "https://github.com/gimlet-io/capacitor"
-  version "0.14.0-rc.2"
+  version "0.14.0-rc.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gimlet-io/capacitor/releases/download/0.14.0-rc.2/next-Darwin-arm64"
-      sha256 "3928e9e27afd7e4139b987df3fb3d4e3dcc75c8bf435c9c32473a3b9b9e1e5aa"
+      url "https://github.com/gimlet-io/capacitor/releases/download/0.14.0-rc.3/next-Darwin-arm64"
+      sha256 "d9726bb96b5aa90c9a85e001c6194415de57a7e2766b6e0498cfcc3c9767ee3a"
     else
       odie "Only arm64 macOS builds are currently provided"
     end
@@ -15,8 +15,8 @@ class Capacitor < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gimlet-io/capacitor/releases/download/0.14.0-rc.2/next-Linux-x86_64"
-      sha256 "c2bc0dbf359459b14dc2351d486bf2ba073a0436ee09385f067474fc16e495af"
+      url "https://github.com/gimlet-io/capacitor/releases/download/0.14.0-rc.3/next-Linux-x86_64"
+      sha256 "73bfee40409348b94a6b22c6ea5edadb4b7a02cf3dcf40b855c80342160d823b"
     else
       odie "Only x86_64 Linux builds are currently provided"
     end
